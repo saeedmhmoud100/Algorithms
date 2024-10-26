9e1649b4e702f23b84f7e9c2e9644fb3bd54ec48
saeedmhmoud100 <saeedmhmoud109@gmail.com>
2024-10-26 20:32:53 +0300

    assignment 1 part b: add far's codes

---
+++ assignment 1/B/fibrecursion.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int fibonacciRecursive(int n) {
+    if (n <= 1)
+        return n;
+    return fibonacciRecursive(n - 1) + fibonacciRecursive(n - 2);
+}
+int main() {
+    int n;
+    cout << "Enter a number: ";
+    cin >> n;
+    // n = 10;
+    cout << "Fibonacci of " << n << " is: " << fibonacciRecursive(n) << endl;
+    return 0;
+}
